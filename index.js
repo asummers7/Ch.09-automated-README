01f062f8eb3f6d43e424e34c1578169af81db080
Alec S <asummers34@gmail.com>
2023-02-16 16:00:15 -0700

    updating Repo

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ ${licenseBadge}
 ${description}
 
 ## Table of Contents
-* [Installation](#-installation)
-* [Usage](#-usage)
-* [License](#-license)
-* [Contributing](#-contributing)
-* [Tests](#-tests)
-* [Questions](#-questions)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
 ## Installation
 ${installation}
